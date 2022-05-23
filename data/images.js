c7c4b5537722eb122d8fd68cd5552a7a1c3df51d
sagar <s.sawuck@providus.mu>
2022-05-23 13:45:57 +0400

    add image data [temp]

---
+++ data/images.js
@@ -0,0 +1,202 @@
+export const itemData = [
+  {
+    img: '/images/3-mamelles-moonrise.jpg',
+    title: '3 Mamelles Moonrise',
+  },
+  {
+    img: '/images/3-mamelles-sunset.jpg',
+    title: '3 Mamelles Sunset',
+  },
+  {
+    img: '/images/3-mamelles-sunset-view.jpg',
+    title: '3 Mamelles Sunset view',
+  },
+  {
+    img: '/images/7-cascades.jpg',
+    title: '7 Cascades',
+  },
+  {
+    img: '/images/400-Pieds.jpg',
+    title: '400 Pieds',
+  },
+  {
+    img: '/images/alma-pieter-view-sunset.jpg',
+    title: 'Sunset Frame Pieter Both',
+  },
+  {
+    img: '/images/alma-sunrise.jpg',
+    title: 'Alma Sunrise',
+  },
+  {
+    img: '/images/alma-sunset-trees.jpg',
+    title: 'Alma Sunset Trees',
+  },
+  {
+    img: '/images/avalon-falls.jpg',
+    title: 'Avalon Falls',
+  },
+  {
+    img: '/images/avalon-falls-portrait.jpg',
+    title: 'Avalon Falls Portrait',
+  },
+  {
+    img: '/images/be-mine-moon.jpg',
+    title: 'Be mine',
+  },
+  {
+    img: '/images/beau-champs-night.jpg',
+    title: 'Beau Champs night',
+  },
+  {
+    img: '/images/benares-sunrise.jpg',
+    title: 'Benares Sunrise',
+  },
+  {
+    img: '/images/benares-sunrise-vibes.jpg',
+    title: 'Benares Sunrise vibes',
+  },
+  {
+    img: '/images/benares-sunset.jpg',
+    title: 'Benares Sunset',
+  },
+  {
+    img: '/images/cloudy-moonrise.jpg',
+    title: 'Cloudy Moonrise',
+  },
+  {
+    img: '/images/cloudy-pieter-portrait.jpg',
+    title: 'Cloudy Pieter',
+  },
+  {
+    img: '/images/distant-boat.jpg',
+    title: 'Shrouded in darkness',
+  },
+  {
+    img: '/images/dreamy-milky-way.jpg',
+    title: 'Dreamy Milky way',
+  },
+  {
+    img: '/images/epic-clouds.jpg',
+    title: 'Epic Clouds',
+  },
+  {
+    img: '/images/fayence-sunrise.jpg',
+    title: 'Fayence Sunrise',
+  },
+  {
+    img: '/images/fools-gold.jpg',
+    title: 'Fool\'s Gold',
+  },
+  {
+    img: '/images/heart-sunset.jpg',
+    title: 'Heart Sunset',
+  },
+  {
+    img: '/images/le-morne-sunset.jpg',
+    title: 'Le Mornes Sunset',
+  },
+  {
+    img: '/images/le-pouce-lightning.jpg',
+    title: 'Le Pouce Lightning',
+  },
+  {
+    img: '/images/le-pouce-moon-composite.jpg',
+    title: 'Le Pouce Moon',
+  },
+  {
+    img: '/images/le-pouce-sunrise.jpg',
+    title: 'Le Pouce Sunrise',
+  },
+  {
+    img: '/images/milky-way-reflection.jpg',
+    title: 'Milky way reflection',
+  },
+  {
+    img: '/images/moody-waves.jpg',
+    title: 'Moody waves',
+  },
+  {
+    img: '/images/moonrise.jpg',
+    title: 'Moonrise',
+  },
+  {
+    img: '/images/moons-eye.jpg',
+    title: 'Moon\'s eye',
+  },
+  {
+    img: '/images/moonshot-fullmoon.jpg',
+    title: 'Fullmoon moonshot',
+  },
+  {
+    img: '/images/ocean-sunset-vibes.jpg',
+    title: 'Ocean sunset vibes',
+  },
+  {
+    img: '/images/pieter-cloudy-sunrise.jpg',
+    title: 'Pieter in the clouds',
+  },
+  {
+    img: '/images/pomponette-night.jpg',
+    title: 'Pomponette Nightshot',
+  },
+  {
+    img: '/images/savinia-cliff.jpg',
+    title: 'Savinia Cliff',
+  },
+  {
+    img: '/images/savinia-cliff-portrait.jpg',
+    title: 'Savinia Cliff Portrait',
+  },
+  {
+    img: '/images/savinia-fiery-sunset.jpg',
+    title: 'Savinia Fiery Sunset',
+  },
+  {
+    img: '/images/savinia-milky-way.jpg',
+    title: 'Savinia Milky way',
+  },
+  {
+    img: '/images/savinia-waves-dark.jpg',
+    title: 'Savinia Waves',
+  },
+  {
+    img: '/images/savinia-waves-pano.jpg',
+    title: 'Savinia Waves panorama',
+  },
+  {
+    img: '/images/shadow-play.jpg',
+    title: 'Shadow Play sunrise',
+  },
+  {
+    img: '/images/signal-sunset.jpg',
+    title: 'Signal Sunset',
+  },
+  {
+    img: '/images/signal-tree.jpg',
+    title: 'Signal Tree',
+  },
+  {
+    img: '/images/stormy-milky-way.jpg',
+    title: 'Stormy Milky way',
+  },
+  {
+    img: '/images/stormy-sunset-vibes.jpg',
+    title: 'Stormy Sunset vibes',
+  },
+  {
+    img: '/images/sunset-clouds-le-pouce.jpg',
+    title: 'Sunset Clouds Le Pouce',
+  },
+  {
+    img: '/images/tamarin-valley-sunset.jpg',
+    title: 'Tamarin Bay sunset',
+  },
+  {
+    img: '/images/tornado-clouds.jpg',
+    title: 'Tornado sunset clouds',
+  },
+  {
+    img: '/images/waterfall-rock-balancing.jpg',
+    title: 'Waterfalls Rock balancing',
+  },
+];
