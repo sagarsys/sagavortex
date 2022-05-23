27d8ab7bd590b60ab846c65657992028d7a7e703
sagar <s.sawuck@providus.mu>
2022-05-23 12:48:26 +0400

    update constants

---
+++ constants.js
@@ -0,0 +1,18 @@
+export const THEME_MODES = {
+  DARK: 'dark',
+  LIGHT: 'light',
+};
+
+export const DARK_COLOR_PALETTE = {
+  BG: '#222629',
+  PRIMARY: '#116466',
+  SECONDARY: '#4e4e50'
+};
+
+export const LIGHT_COLOR_PALETTE = {
+  BG: '#d1e8e2',
+  PRIMARY: '#d1e8e2',
+  SECONDARY: '#FFCB9A'
+};
+
+export const DRAWER_WIDTH = 240;
