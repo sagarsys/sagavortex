14cb0abe4aabfbc4225b4feb065b47eeff04bf9e
sagar <s.sawuck@providus.mu>
2022-05-23 15:01:31 +0400

    add social media data

---
+++ data/social-links.js
@@ -0,0 +1,27 @@
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import EmailIcon from '@mui/icons-material/Email';
+import LinkIcon from '@mui/icons-material/Link';
+
+export const SocialLinks = [
+  {
+    name: 'Instagram',
+    link: 'https://instagram.com/sagavortex',
+    icon: InstagramIcon
+  },
+  {
+    name: 'Facebook',
+    link: 'https://facebook.com/sagarsys',
+    icon: FacebookIcon
+  },
+  {
+    name: 'Email',
+    link: 'mailto:sagar.sawuck@gmail.com?subject=Photography%20Inquiry',
+    icon: EmailIcon
+  },
+  {
+    name: 'Web development',
+    link: 'https://sagarsys.net',
+    icon: LinkIcon
+  },
+]
