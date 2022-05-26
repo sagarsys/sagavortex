c20c4f1fa1536daed5534b9062ccff70b541bc14
sagar <s.sawuck@providus.mu>
2022-05-26 14:10:55 +0400

    semantic html => add nav

--- components/SideMenuNav.js
+++ components/SideMenuNav.js
@@ -13,34 +13,36 @@ function SideMenuNav({ open }) {
   const router = useRouter()
   const handleClick = (e, link) => { e.preventDefault(); router.push(link); };
   return (
-      <List>
-        {SITE_MENU_ITEMS.map(({ id, label, icon, link}) => {
-          const Icon = icon;
-          return (
-              <ListItem key={id} disablePadding sx={{ display: 'block' }}>
-                <ListItemButton
-                    sx={{
-                      minHeight: 48,
-                      justifyContent: open ? 'initial' : 'center',
-                      px: 2.5,
-                    }}
-                    onClick={(e) => handleClick(e, link)}
-                >
-                      <ListItemIcon
-                          sx={{
-                            minWidth: 0,
-                            mr: open ? 3 : 'auto',
-                            justifyContent: 'center',
-                          }}
-                      >
-                        <Icon />
-                      </ListItemIcon>
-                      <ListItemText primary={label} sx={{ opacity: open ? 1 : 0 }} />
-                </ListItemButton>
-              </ListItem>
-          )
-        })}
-      </List>
+      <nav>
+        <List>
+          {SITE_MENU_ITEMS.map(({ id, label, icon, link}) => {
+            const Icon = icon;
+            return (
+                <ListItem key={id} disablePadding sx={{ display: 'block' }}>
+                  <ListItemButton
+                      sx={{
+                        minHeight: 48,
+                        justifyContent: open ? 'initial' : 'center',
+                        px: 2.5,
+                      }}
+                      onClick={(e) => handleClick(e, link)}
+                  >
+                    <ListItemIcon
+                        sx={{
+                          minWidth: 0,
+                          mr: open ? 3 : 'auto',
+                          justifyContent: 'center',
+                        }}
+                    >
+                      <Icon />
+                    </ListItemIcon>
+                    <ListItemText primary={label} sx={{ opacity: open ? 1 : 0 }} />
+                  </ListItemButton>
+                </ListItem>
+            )
+          })}
+        </List>
+      </nav>
   );
 }
 
