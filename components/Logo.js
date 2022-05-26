4108244d011592e57cff2dadbb005a3d38f79bf7
sagar <s.sawuck@providus.mu>
2022-05-26 14:10:30 +0400

    reformat

--- components/Logo.js
+++ components/Logo.js
@@ -3,7 +3,13 @@ import Image from 'next/image';
 
 const Logo = ({ size = 50 }) => {
   return (
-      <Image src="/logo-sagar-sys-black.png" alt="SagaVortex Logo" width={size} height={size} />
+      <Image
+          src="/logo-sagar-sys-black-sm.png"
+          alt="SagaVortex Photography Logo"
+          width={size}
+          height={size}
+          priority
+      />
   );
 };
 
