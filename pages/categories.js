7ef494853176147c57e1c62a2bdf1a1241999b31
sagar <s.sawuck@providus.mu>
2022-05-26 14:23:31 +0400

    fix page due to layout changes

--- pages/categories.js
+++ pages/categories.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import styles from '../styles/Home.module.css';
 import Head from 'next/head';
-import Layout from '../components/Layout';
 import Typography from '@mui/material/Typography';
 
 const Categories = () => {
   return (
-      <div className={styles.container}>
+      <>
         <Head>
           <title>Categories - SagaVortex Photography</title>
         </Head>
-        <Layout>
-          <Typography paragraph>
-            Categories
-          </Typography>
-        </Layout>
-      </div>
+        <Typography paragraph>
+          Categories
+        </Typography>
+      </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Layout from '../components/Layout';
 import HomeGallery from '../components/HomeGallery';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>SagaVortex Photography</title>
       </Head>
-      <Layout>
-        <HomeGallery />
-      </Layout>
-    </div>
+      <HomeGallery />
+    </>
   )
 }
