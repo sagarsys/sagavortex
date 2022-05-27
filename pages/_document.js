92ac8deddc3484ba2f6a7919320bf2896f9bde87
sagar <s.sawuck@providus.mu>
2022-05-27 16:21:40 +0400

    add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,10 @@ export default class MyDocument extends Document {
             <meta name="description" content="Professional Landscape Photographer - Visual Vortex" />
             {/* PWA primary color */}
             <meta name="theme-color" content={'#556cd6'} />
-            <link rel="shortcut icon" href="/favicon.ico" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="manifest" href="/site.webmanifest" />
             <link
                 rel="stylesheet"
                 href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
